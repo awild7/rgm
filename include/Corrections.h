f2b467b8f02eb1f3512f074935e2a6f0ba277e43
Andrew Denniston <awild@mit.edu>
2026-02-19 16:27:58 -0500

    New corrections file for the new note

--- include/Corrections.h
+++ include/Corrections.h
@@ -64,68 +64,71 @@ double params_EnergyLoss_CD[3][2]={{-0.00555986,-6.06201e-05},
 				   {0.00695634,8.24535e-05},
 				   {0.00155103,1.74283e-05}};
 
-double params_Momentum_negparts_FD[6][4][4]={{{-0.00214337,-0.0202413,19.9966,19.9994},
-					      {0.0673539,0.0273376,5.8074,24.9818},
-					      {145.567,50.4663,4,16.4935},
-					      {0.470742,-1.15944,4.00003,19.082}},
-					     {{0.0376835,0.0166851,8.23076,24.9989},
-					      {0.0235114,0.00170224,12.7113,15.3318},
-					      {140.222,31.2228,1.5,13.9711},
-					      {-0.0372922,-1.9027,1.5,20.4744}},
-					     {{0.0205703,0.0146918,5.7492,19.9938},
-					      {0.0711701,0.0328288,4.29665,24.9937},
-					      {115.006,22.6111,1.5,23.0711},
-					      {2.67023,1.55987,4,15.5237}},
-					     {{-0.00204655,-0.0207699,19.9975,19.9986},
-					      {0.0588647,0.0229838,5.62843,24.9975},
-					      {142.111,66.05,8.64234,17.5045},
-					      {2.25598,1.26862,5.72383,13.3042}},
-					     {{0.0424364,0.0494246,19.2225,23.2837},
-					      {0.0489891,0.0145193,8.31129,24.9937},
-					      {148.98,65.4393,4,10},
-					      {1.01252,-0.181366,4.00001,10.0004}},
-					     {{0.0159458,0.0106683,1.51661,18.2011},
-					      {0.0591658,0.0269474,4.17777,24.9879},
-					      {125.981,50.2509,4,15.6732},
-					      {0.97521,0.672086,4.00002,24.9999}}};
+const double params_Momentum_negparts_FD[6][4][4]={{{0.257158,-0.136574,4,25},
+						    {1.79326,0.729487,4,25},
+						    {121.024,40.091,5.00025,15},
+						    {0.15236,-1.30429,1.00004,19.1085}},
+						   {{0.781339,0.526124,4.35603,21.3139},
+						    {1.22716,0.448927,4,24.2824},
+						    {169.925,53.0067,4,17.9088},
+						    {-0.139585,-1.28625,1.08963,19.6235}},
+						   {{1.68681,0.96684,5.33823,23.4936},
+						    {2.07862,1.13561,9.31451,24.8375},
+						    {91.215,11.2227,4.00005,15},
+						    {-3.31088,-0.431251,5.92834,15.0001}},
+						   {{1.63967,0.683461,4,23.4088},
+						    {0.833452,0.668007,4,20.6145},
+						    {129.332,42.812,4.85292,18.6698},
+						    {-3.10738,-0.605016,1,20.4228}},
+						   {{1.34151,0.990672,10,22.6629},
+						    {0.144001,-0.0533304,9.12876,15.0012},
+						    {56.678,-7.6276,4,21.8985},
+						    {-1.03723,-0.831154,3.09479,15}},
+						   {{1.29883,0.888824,6.47301,21.2259},
+						    {0.194675,-0.127275,4.00001,15},
+						    {44.0007,-8.33546,4,15.1237},
+						    {-0.439147,-0.599292,1,16.0641}}};
 
-double params_Momentum_posparts_FD[6][4][4]={{{0.0994698,0.06203,4.00001,37.9995},
-					      {0.083833,0.000338038,20.8983,30.0176},
-					      {102.196,-14.058,4,27.4109},
-					      {-2.70134,-0.335195,4.00079,37.9999}},
-					     {{0.100609,0.0714968,4.01151,37.9997},
-					      {0.0674467,0.0195951,4.53667,26.0002},
-					      {101.988,-14.4089,4,27.1633},
-					      {-2.66578,-0.241631,4,38}},
-					     {{0.131204,0.0895802,3.53093,37.9672},
-					      {0.0189443,0.0420609,19.8174,29.9951},
-					      {107.198,26.2374,2,38},
-					      {-2.92875,-0.0467252,2.00003,32.0315}},
-					     {{0.0902545,0.0582306,2.02946,37.0886},
-					      {0.0368395,0.00706995,13.4986,26.001},
-					      {98.4986,-12.3151,2.97928,36.2819},
-					      {-1.8963,-0.652783,5.99785,34.34}},
-					     {{0.139861,0.104554,5.27712,37.924},
-					      {-0.0329756,-0.0325642,4.00869,26.0012},
-					      {106.329,16.3735,4,33.941},
-					      {0.241212,-1.18799,23.8338,37.9821}},
-					     {{0.146429,0.0995005,4.14491,36.9621},
-					      {-0.0281189,-0.0121676,4.36001,30.4651},
-					      {107.814,28.5356,4,33.207},
-					      {-0.324309,-2.82037,31.3704,32.1691}}};
+const double params_Momentum_posparts_FD[6][4][4]={{{-0.705061,1.01168,5,26.0005},
+						    {3.48797,0.754098,1.92478,30.9666},
+						    {114.46,8.69429,3,26},
+						    {-1.7963,1.01494,5,26.5376}},
+						   {{-0.0388399,0.831155,2.26375,27.1916},
+						    {3.49646,1.03748,1.80573,27.729},
+						    {161.625,21.6904,3,27.6331},
+						    {-1.78998,0.611645,5,28.0376}},
+						   {{-0.698553,1.3504,1.24212,26.4306},
+						    {-0.442019,1.66664,4.99999,26.0007},
+						    {200,-16.5106,4.97897,34},
+						    {2.21317,-1.01685,1.93538,28.1037}},
+						   {{0.0864055,1.33218,5,28.3018},
+						    {1.80606,1.79593,4.23224,27.6494},
+						    {167.639,19.6429,3,31.0384},
+						    {-1.15901,0.465383,1.00009,27.7387}},
+						   {{-1.00385,0.387042,2.4097,26.0041},
+						    {-1.7593,-0.514224,1.00013,26.0001},
+						    {146.135,20.5291,3,28.1081},
+						    {1.28355,-1.12501,1.42483,26.0018}},
+						   {{0.902805,1.11944,3,34},
+						    {-2.32694,-0.909035,1,31.8645},
+						    {108.115,22.0863,4.88316,26.0001},
+						    {2.16539,0.371047,1.00015,26}}};
 
-double params_Momentum_CD[2][4]={{0.0585217,0.00989627,5.00872,42.3376},
-				 {-1.11617,0.0736438,5.0042,48.9754}};
+double params_Momentum_CD[5][4]={{-0.250757,1.68519,14.261,74.5813},
+				 {-0.95819,0.234407,6.15029,45.6566},
+				 {-1.06401,2.89001,18.8068,45},
+				 {0.669503,-1.43197,6.56704,45},
+				 {0.285612,0.561206,5.00003,58.4323}};
 
 
-double params_Smear_FD[6][6]={{1.27164,-0.103322,0.00374144,0.22998,0.00155623,0.000888604},
-		      {0.716006,-0.0386559,0.00203629,0.557439,-0.0323154,0.00163883},
-		      {1.10877,-0.0853149,0.00335627,-0.465281,0.0735502,-0.000816378},
-		      {0.916793,-0.0645254,0.0026928,0.267525,-0.00692347,0.00114717},
-		      {1.18955,-0.0993153,0.00359098,0.262575,-0.00198241,0.000950544},
-		      {0.486033,-0.0162104,0.0015784,-0.143655,0.0362722,0.000199701}};
+double params_Smear_FD[6][6]={{0.242269,0.012285,0.000722975,0.229986,0.00155551,0.00088862},
+{-0.0331068,0.0452348,-4.87551e-05,0.557431,-0.0323144,0.00163881},
+{-0.0781403,0.0453914,0.000252317,-0.465264,0.0735487,-0.000816354},
+{0.0767921,0.0289579,0.000467256,0.267525,-0.00692348,0.00114717},
+{-0.0501808,0.0404422,0.000173173,0.262579,-0.00198293,0.000950558},
+  {0.167938,0.0297138,0.000191341,-0.143651,0.0362719,0.000199702}};
 
-double params_Smear_CD[8]={10.049,3.07365,0.461349,1.47116,-0.832429,1.45479,1.67907,3.90999};
+double params_Smear_CD[2]={12.4637,6.76708};
 
 /*
 //Vertex Smearing
@@ -157,6 +160,10 @@ double Function_Trig(double x, double A, double B, double C, double D){
   return A + B*sin((x*2*M_PI/C)+D); 
 }
 
+double Function_Cosine(double x, double A, double B, double C, double D){
+  return A + (B*cos((M_PI/2)*(1/C)*(x-D)));
+}
+
 double Function_TrigFixedPeriodFixedOffset(double x, double A, double B){
   return A*sin((x*2*M_PI/180)+B); 
 }
@@ -167,6 +174,13 @@ double Function_TrigFixedPeriod(double x, double A, double B, double C){
 
 double Function_TrigTripleFixedPeriod(double x, double A, double B, double C, double D, double E, double F, double G){
   return A + B*sin((x*1*M_PI/180)+C) + D*sin((x*2*M_PI/180)+E) + F*sin((x*3*M_PI/180)+G); 
+}
+
+double Function_TrigQuadFixedPeriod(double x, double A, double B, double C, double D, double E){
+  return A +
+    C*sin((x*1*M_PI/180)+ B) +
+    D*sin((x*2*M_PI/180)+ B) +
+    E*sin((x*3*M_PI/180)+ B);
 }
 
 double Function_Algebraic1(double x, double A, double B){
@@ -197,8 +211,8 @@ double SubtractQuadriture_FD(double x, double A, double B, double C, double D, d
   return 0.1;
 }
 
-double SubtractQuadriture_CD(double x, double A, double B, double C, double D, double E, double F, double G, double H){
-  double X = Function_TrigTripleFixedPeriod(x,A,B,C,D,E,F,G)*Function_TrigTripleFixedPeriod(x,A,B,C,D,E,F,G) - H*H;
+double SubtractQuadriture_CD(double x, double A, double B){
+  double X = A*A - B*B;
   if(X>0.01){
     return sqrt(X); 
   }
@@ -276,6 +290,7 @@ void SetLorentzVector_MomentumCorrection(TLorentzVector &p4, clas12::region_part
   double theta = v3.Theta()*180/M_PI;
   double phi = v3.Phi()*180/M_PI;
   double M = p4.M();        
+  double correction = 0;
   if(p->getRegion()==FD){
     int sector = p->getSector();
     double shift = 0;
@@ -290,17 +305,22 @@ void SetLorentzVector_MomentumCorrection(TLorentzVector &p4, clas12::region_part
       else{
 	params[i] = Function_Erf(theta,params_Momentum_posparts_FD[sector-1][i][0],params_Momentum_posparts_FD[sector-1][i][1],params_Momentum_posparts_FD[sector-1][i][2],params_Momentum_posparts_FD[sector-1][i][3]);
       }
-    }    
-    mom+=Function_Trig(phi,params[0],params[1],params[2],params[3]);
+    }
+    correction=Function_Trig(phi,params[0],params[1],params[2],params[3]);
+    mom/=(1-(correction/100));
   }
   else if(p->getRegion()==CD){
-    double params[2];
-    for(int i = 0; i < 2; i++){
-      params[i] = Function_Erf(theta,params_Momentum_CD[i][0],params_Momentum_CD[i][1],params_Momentum_CD[i][2],params_Momentum_CD[i][3]);
+    double params[5];
+    for(int i = 0; i < 5; i++){
+      if(i==0){
+	params[i] = Function_Cosine(theta,params_Momentum_CD[i][0],params_Momentum_CD[i][1],params_Momentum_CD[i][2],params_Momentum_CD[i][3]);
+      }
+      else{
+	params[i] = Function_Erf(theta,params_Momentum_CD[i][0],params_Momentum_CD[i][1],params_Momentum_CD[i][2],params_Momentum_CD[i][3]);
+      }
     }
-    mom+=Function_TrigFixedPeriodFixedOffset(phi,params[0],params[1]);
-
-    
+    correction=Function_TrigQuadFixedPeriod(phi,params[0],params[1],params[2],params[3],params[4]);
+    mom/=(1-(correction/100));   
   }
   else{
     cout<<"Problem\n\n\n\n\n\n\n\n\n";
@@ -322,7 +342,7 @@ void SetLorentzVector_MomentumSimulationSmear(TLorentzVector &p4, clas12::region
     mom*=thisRand->Gaus(1.0,smear);
   }
   else if(p->getRegion()==CD){
-    double smear = 0.01*SubtractQuadriture_CD(phi,params_Smear_CD[0],params_Smear_CD[1],params_Smear_CD[2],params_Smear_CD[3],params_Smear_CD[4],params_Smear_CD[5],params_Smear_CD[6],params_Smear_CD[7]);
+    double smear = 0.01*SubtractQuadriture_CD(phi,params_Smear_CD[0],params_Smear_CD[1]);
     mom*=thisRand->Gaus(1.0,smear);
   }
   else{
